5153b226bff21764f622e959365068083225077a
Toni Schmidbauer <toni@stderr.at>
2021-10-14 18:34:00 +0200

    Start testing the JDBC connection

--- hb-01-one-to-one-uni/src/resources/V1_create_database.sql
+++ hb-01-one-to-one-uni/src/resources/V1_create_database.sql
@@ -1,1 +1,31 @@
-create table
+DROP SCHEMA IF EXISTS "hb-01-one-to-one-uni";
+
+CREATE SCHEMA "hb-01-one-to-one-uni";
+
+set search_path hb-01-one-to-one-uni, public;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS "instructor_detail";
+
+CREATE TABLE `instructor_detail` (
+  id serial,
+  youtube_channel varchar(128) DEFAULT NULL,
+  hobby varchar(45) DEFAULT NULL,
+  PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS "instructor";
+
+CREATE TABLE `instructor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `instructor_detail_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_DETAIL_idx` (`instructor_detail_id`),
+  CONSTRAINT `FK_DETAIL` FOREIGN KEY (`instructor_detail_id`) REFERENCES `instructor_detail` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+SET FOREIGN_KEY_CHECKS = 1;
