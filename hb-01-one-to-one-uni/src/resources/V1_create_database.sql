d210cf9a80486a82d677cc73f11a7975e942495d
Toni Schmidbauer <toni@stderr.at>
2021-10-24 17:58:47 +0200

    update create database script to pgsql

--- hb-01-one-to-one-uni/src/resources/V1_create_database.sql
+++ hb-01-one-to-one-uni/src/resources/V1_create_database.sql
@@ -1,14 +1,12 @@
-DROP SCHEMA IF EXISTS "hb-01-one-to-one-uni";
+DROP SCHEMA IF EXISTS "hb-01-one-to-one-uni" cascade;
 
 CREATE SCHEMA "hb-01-one-to-one-uni";
 
-set search_path hb-01-one-to-one-uni, public;
+set search_path to "hb-01-one-to-one-uni", public;
 
-SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE IF EXISTS "instructor_detail" cascade ;
 
-DROP TABLE IF EXISTS "instructor_detail";
-
-CREATE TABLE `instructor_detail` (
+CREATE TABLE instructor_detail (
   id serial,
   youtube_channel varchar(128) DEFAULT NULL,
   hobby varchar(45) DEFAULT NULL,
@@ -17,15 +15,12 @@ CREATE TABLE `instructor_detail` (
 
 DROP TABLE IF EXISTS "instructor";
 
-CREATE TABLE `instructor` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(45) DEFAULT NULL,
-  `last_name` varchar(45) DEFAULT NULL,
-  `email` varchar(45) DEFAULT NULL,
-  `instructor_detail_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_DETAIL_idx` (`instructor_detail_id`),
-  CONSTRAINT `FK_DETAIL` FOREIGN KEY (`instructor_detail_id`) REFERENCES `instructor_detail` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+CREATE TABLE instructor (
+  id serial,
+  first_name varchar(45) DEFAULT NULL,
+  last_name varchar(45) DEFAULT NULL,
+  email varchar(45) DEFAULT NULL,
+  instructor_detail_id int references instructor_detail(id)
+);
 
-SET FOREIGN_KEY_CHECKS = 1;
+
