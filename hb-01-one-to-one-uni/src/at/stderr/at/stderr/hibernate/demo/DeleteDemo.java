8c26261f88f1d4688ae5152ddc5a736e14124857
Toni Schmidbauer <toni@stderr.at>
2021-11-01 18:51:06 +0100

    finished sectino 23

---
+++ hb-01-one-to-one-uni/src/at/stderr/at/stderr/hibernate/demo/DeleteDemo.java
@@ -0,0 +1,44 @@
+package at.stderr.at.stderr.hibernate.demo;
+
+import at.stderr.hibernate.demo.entity.Instructor;
+import at.stderr.hibernate.demo.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteDemo {
+
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            // get instructor by primary key
+            int theId = 3;
+            Instructor tempInstructor = session.get(Instructor.class, theId);
+
+            System.out.println("Found inststructor:" + tempInstructor);
+
+            if(tempInstructor != null) {
+                System.out.println("Deleting: " + tempInstructor);
+
+                // will also delelte the details object
+                // because of CascadeType.ALL
+                session.delete(tempInstructor);
+            }
+
+            // commit transaction
+            session.getTransaction().commit();
+        }
+        finally {
+            factory.close();
+        }
+    }
+}
