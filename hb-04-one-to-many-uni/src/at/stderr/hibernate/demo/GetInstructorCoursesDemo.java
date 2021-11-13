5ae085ba89b19354ccf275e4f902f753f91dad8f
Toni Schmidbauer <toni@stderr.at>
2021-11-13 12:21:54 +0100

    import hb-04-on-to-many-uni

---
+++ hb-04-one-to-many-uni/src/at/stderr/hibernate/demo/GetInstructorCoursesDemo.java
@@ -0,0 +1,47 @@
+package at.stderr.hibernate.demo;
+
+import at.stderr.hibernate.demo.entity.Course;
+import at.stderr.hibernate.demo.entity.Instructor;
+import at.stderr.hibernate.demo.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class GetInstructorCoursesDemo {
+
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+            // create the objects
+
+            // start the transaction
+            session.beginTransaction();
+
+            // get instructor from database
+            int theID = 1;
+            Instructor tempInstructor = session.get(Instructor.class, theID);
+
+            System.out.println("Instructor: " + tempInstructor);
+
+            // get courses for the instructor
+            System.out.println("Courses: " + tempInstructor.getCourses());
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            System.out.printf("Done!");
+        }
+        finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
