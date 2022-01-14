6913a2edf58770b64450eceeb83a0e82402b3b95
Toni Schmidbauer <toni@stderr.at>
2022-01-14 18:24:19 +0100

    update

--- spring-demo-aop-pointcut-declarations/src/main/java/at/stderr/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-pointcut-declarations/src/main/java/at/stderr/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -3,14 +3,23 @@ package at.stderr.aopdemo.aspect;
 import at.stderr.aopdemo.dao.AccountDAO;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
 
-    @Before("execution(* at.stderr.aopdemo.dao.*.*(..))")
+    @Pointcut("execution(* at.stderr.aopdemo.dao.*.*(..))")
+    private void forDAOPackage() { /* Empty because of Pointcut */ }
+
+    @Before("forDAOPackage()")
     public void beforeAddAccountAdvice() {
-        System.out.println("\n=======>>>> Executing @Before advice on method");
+        System.out.println("\n=======>>>> Executing @Before advice on method with pointcut declaration");
+    }
+
+    @Before("forDAOPackage()")
+    public void beforeApiAnalytics() {
+        System.out.println("\n=======>>>> Performing API analytics");
     }
 }
