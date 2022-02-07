a62937c1803c266609f3140d0b047b339b62f1c6
Toni Schmidbauer <toni@stderr.at>
2022-02-07 18:09:03 +0100

    section 48 done

---
+++ Spring-Security/spring-security-demo-03-custom-login-form/src/main/java/at/stderr/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,33 @@
+package at.stderr.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser(users.username("john").password("test1234").roles("EMPLOYEE"))
+                .withUser(users.username("marry").password("test1234").roles("MANAGER"))
+                .withUser(users.username("susane").password("test1234").roles("ADMIN"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/showMyLoginPage")
+                .loginProcessingUrl("/authenticateTheUser")
+                .permitAll();
+    }
+}
