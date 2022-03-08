77588847340e0af9a90c12aca22861b40cf2ae16
Toni Schmidbauer <toni@stderr.at>
2022-03-08 21:45:02 +0100

    section 58

---
+++ Spring-REST/spring-rest-demo/src/main/java/at/stderr/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,12 @@
+package at.stderr.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("at.stderr.springdemo")
+public class DemoAppConfig {
+
+}
