6db87811c886528f7770c8c8bd7eb4c3d53b744e
Toni Schmidbauer <toni@stderr.at>
2022-03-14 18:55:57 +0100

    section 60 done

--- Spring-REST/spring-rest-demo/src/main/java/at/stderr/springdemo/rest/StudentRestController.java
+++ Spring-REST/spring-rest-demo/src/main/java/at/stderr/springdemo/rest/StudentRestController.java
@@ -2,24 +2,33 @@ package at.stderr.springdemo.rest;
 
 import at.stderr.springdemo.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+    private final List<Student> students = new ArrayList<>();
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        var students = new ArrayList<Student>();
-
+    @PostConstruct
+    public void loadData() {
         students.add(new Student("Poornima", "Patel"));
         students.add(new Student("Mario", "Rossi"));
         students.add(new Student("Marry", "Smith"));
+    }
 
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+        return students.get(studentId);
     }
 }
