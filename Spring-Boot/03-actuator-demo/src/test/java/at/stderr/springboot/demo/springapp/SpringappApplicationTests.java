43e942d89106ce51e8296ee14f3a617330eb1c2f
Toni Schmidbauer <toni@stderr.at>
2022-03-28 18:52:55 +0200

    import actuator test file

---
+++ Spring-Boot/03-actuator-demo/src/test/java/at/stderr/springboot/demo/springapp/SpringappApplicationTests.java
@@ -0,0 +1,13 @@
+package at.stderr.springboot.demo.springapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
